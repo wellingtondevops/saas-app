b7903adbc72018255aecf3b46bc0f2742dd3c7f0
wellingtodevops <wellingtondevops@gmail.com>
2018-09-17 18:48:18 +0000

    models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
+         
+          acts_as_universal_and_determines_account
+          has_one :member, :dependent => :destroy
+
+
 end
