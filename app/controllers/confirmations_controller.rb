209c7cc49cf8fcb471501291371104b47e00d3af
wellingtodevops <wellingtondevops@gmail.com>
2018-09-23 15:31:12 +0000

    make I removed the line config / application.rb -> config.load_defaults 5.1

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -54,6 +54,8 @@ class ConfirmationsController <Milia::ConfirmationsController
     private
     
     def set_confirmable()
-       @confirmable = User.find_or_initialize_with_error_by(:confirmation_token, params[confirmation_token])
+
+    @confirmable = User.find_or_initialize_with_error_by(:confirmation_token, params[:confirmation_token])
+
     end
 end
